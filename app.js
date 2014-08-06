6c322b6dd08d7b66adc94ff630fa6149be990faa
msalli <alli.cernoch@gmail.com>
2014-08-05 20:21:32 -0700

    created routes folder, added site and eva files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+    //all modules
 var express = require('express'),
     Instagram = require('instagram-node-lib'),
     bodyParser = require('body-parser'),
@@ -5,8 +6,10 @@ var express = require('express'),
     _ = require('lodash'),
     OAuth = require('oauth'),
     tumblr = require('tumblr.js'),
+    
     //ROUTES
     site = require('./routes/site'),
+    eva = require('./routes/eva'),
     app = express();
 
 
@@ -15,6 +18,9 @@ require('locus');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(site);
+app.use(eva);
+
+
 
 //set up oauth connection
 var oauth = new OAuth.OAuth(
@@ -39,62 +45,14 @@ var client = tumblr.createClient({
 Instagram.set('client_id', process.env.INSTAGRAM_KEY);
 Instagram.set('client_secret', process.env.INSTAGRAM_SECRET);
 
-// set up function to get all tweets from user
-var getTweets = function (url, callback) {
-  oauth.get(url, null, null, function (e, data, res){
-    var allTweets = JSON.parse(data);
-    // console.log("This is all tweets", allTweets);
-    // console.log("This is data", data);
-    callback(allTweets);
-  });
-}; //closing for getTweets
 
 
-  //set up pages
-  
-  //welcome page setup
-  app.get('/', function (req, res) {
-    res.render('index.ejs');
-  });
-
-  //login page setup
-  app.get('/index/login', function (req, res) {
-    res.render('login');
-  });
-
-  //signup page setup
-  app.get('/index/signup', function (req, res) {
-    res.render('signup');
-  });
-
-  //home page setup
-  app.get('/home', function (req, res) {
-    res.render('home');
-  });
-
-  //eva tweets page setup
-  app.get('/eva/tweets', function (req, res) {
-    var url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=evachen212";
-      getTweets(url, function (allTweets) {
-      //eval(locus);
-      res.render('eva/tweets', {tweets: allTweets});
-    });
-  });
 
 
-//eva insta page set up
-app.get('/eva/insta', function (req, res) {
-  Instagram.users.recent({ user_id: 2098900,
-    complete:function(user) {
-      console.log(user);
-      // eval(locus);
-      res.render('eva/insta', {users: user});
-    }});
+
+
+
+
+app.listen(3000, function() {
+console.log("runnin shit on port 3000");
 });
-
-
-
-
-    app.listen(3000, function() {
-      console.log("runnin shit on port 3000");
-    });
--- routes/site.js
+++ routes/site.js
@@ -1,9 +1,27 @@
 var express = require('express'),
     router = express.Router();
 
+  //set up pages
+  
+  //welcome page setup
+  router.get('/', function (req, res) {
+    res.render('index.ejs');
+  });
 
+  //login page setup
+  router.get('/index/login', function (req, res) {
+    res.render('login');
+  });
 
+  //signup page setup
+  router.get('/index/signup', function (req, res) {
+    res.render('signup');
+  });
 
+  //home page setup
+  router.get('/home', function (req, res) {
+    res.render('home');
+  });
 
 
 
