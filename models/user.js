7b3a14dbdb226a38b5c409dbc91cd7c6850e6e23
msalli <alli.cernoch@gmail.com>
2014-08-05 23:41:57 -0700

    add classMethods for encryptPass, comparePass, and createNewUser. npm install: passport, passport-local

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
-
+var bcrypt = require('bcrypt');
+var salt = bcrypt.genSaltSync(10);
+var passport = require('passport');
+var passportLocal = require('passport-local');
 
 
 module.exports = function (sequelize, DataTypes) {
@@ -25,6 +28,44 @@ module.exports = function (sequelize, DataTypes) {
         notEmpty: true
       }
     }
+  }, //closing var User, second object
+    {
+      classMethods: {
+        //encrypt pass
+        encryptPass: function (password) {
+          var hash = bcrypt.hashSync(password, salt);
+          return hash;
+        },
+        //compare pass
+        comparePass: function (userpass, dbpass) {
+          return bcrypt.compareSync(userpass, dbpass);
+        },
+        //create new user
+        createNewUser: function (username, password, err, success) {
+          if (password.length < 6) {
+            err({message: "Password must be more than 6 characters."});
+          }
+          else {
+            User.create({
+              username: username, //referencing param
+              password: User.encryptPass(password) //password in () is param
+            }).error(function(error) {
+              console.log(error);
+              if(error.username) {
+                err({message: "You username must be at least 6 characters."});
+              }
+              else {
+                err({message: "An account with that username already exists!"});
+              }
+            }).success(function (user) {
+              success({message: "It's official! Go ahead and login."});
+            });
+          }
+        } //closing createNewUser
+      } //closing inner classMethods
+    } //closing outer classMethods
+  ); //closing User function
 
-  }) //closing User function
 }; //closing module.exports function
+
+
